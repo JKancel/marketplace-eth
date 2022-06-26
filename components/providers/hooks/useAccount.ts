9487ae1bead0e604e77417346e25699972b1d88b
Joffrey Kancel <joffrey.kancel@gmail.com>
2022-06-26 18:37:58 +0200

    hash Admin account with keccak256

--- components/providers/hooks/useAccount.ts
+++ components/providers/hooks/useAccount.ts
@@ -3,12 +3,12 @@ import Web3 from "web3";
 import { useHooks } from "../web3";
 import useSWR from "swr";
 
-const adminAddresses : { [key: string]: boolean } = {
-  "0x803aE903178f21AD36955d082f4F3dDE75A64D7b": true,
-}
+const adminAddresses: { [key: string]: boolean; } = {
+  "0xc8f8a43d75617377230a387977f727e0f44d44215e33f0f756f97093929e4326": true,
+};
 
 export const createUseAccount = (web3: Web3, provider: any) => (): { account: any; } => {
-  const { data, mutate, ...rest } = useSWR(() => 
+  const { data, mutate, ...rest } = useSWR(() =>
     web3 ? "web3/accounts" : null, // trigger the fetcher when web3 is ready
     async () => {
       const accounts = await web3?.eth.getAccounts();
@@ -22,13 +22,13 @@ export const createUseAccount = (web3: Web3, provider: any) => (): { account: an
     providerInstance && providerInstance.on("accountsChanged", (accounts: any[]) => mutate(accounts[0] ?? null));
   }, [mutate, providerInstance]);
 
-  return { 
+  return {
     account: {
       data,
-      isAdmin: data && !!adminAddresses[data],
+      isAdmin: data && !!adminAddresses[web3?.utils.keccak256(data)],
       mutate,
       ...rest
-    } 
+    }
   };
 };
 
